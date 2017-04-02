e91045659a0725aba3916222294dd41b18722252
= <jovic.i24@gmail.com>
2017-04-02 15:03:37 +0200

    minor clean up

--- src/song_recommender/core.clj
+++ src/song_recommender/core.clj
@@ -3,12 +3,6 @@
             [song-recommender.data-manipulation :as data]))
 
 
-(println (data/normalize_features (first global/raw-data) global/numeric-features))
-
-(println (data/extract-features (first global/raw-data) '(:remote_id :genres)))
-
-(println (data/row->normalized_row (first global/raw-data) global/non-numeric-features global/numeric-features))
-
 (def normalized-data
   (map #(data/row->normalized_row % global/non-numeric-features global/numeric-features) global/raw-data))
 
