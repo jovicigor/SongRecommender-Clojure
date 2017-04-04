d1b8c45c6fc49c5a30a6b21a57c5aaba58888e20
= <jovic.i24@gmail.com>
2017-04-04 19:21:57 +0200

    implement centroid calculation

--- src/song_recommender/clustering.clj
+++ src/song_recommender/clustering.clj
@@ -9,6 +9,7 @@
   (euclidean-distance (map #(% map1) features)
                       (map #(% map2) features)))
 
+;see if the filter is necessary ??
 (defn find-closest-from-items [item items distance features]
   (let [items-items-without-item (filter #(not= (:remote_id %) (:remote_id item)) items)]
     (first (sort-by #(distance % item features) items-items-without-item))))
@@ -17,6 +18,8 @@
 (defn assign-to-means [means items distance features]
   (group-by #(:remote_id (find-closest-from-items % means distance features)) items))
 
+
+;this method should be in average-item method
 (defn average-point [items features]
   (into {}
         (map
@@ -27,4 +30,26 @@
   (let [average-point (average-point items features)]
     (find-closest-from-items average-point items distance features)))
 
+;here you should map values to average-item
+(defn find-new-means [old-means items distance features]
+  (println "looking for new means")
+  (map #(average-item % features distance) (vals (assign-to-means old-means items distance features))))
 
+(defn find-new-means-if-different [old-means items distance features]
+  (let [new-means (find-new-means old-means items distance features)]
+    (if (every? true? (map #(.contains (map :remote_id old-means) %) (map :remote_id new-means)))
+      old-means
+      (find-new-means-if-different old-means items distance features))))
+
+
+(defn are-means-same [old-means new-means]
+  (every? true? (map #(.contains (map :remote_id old-means) %) (map :remote_id new-means))))
+
+(defn calculate-centroids [old-means items distance features max-iterations]
+  (let [new-means (find-new-means old-means items distance features)]
+    (while (not (are-means-same old-means new-means))
+      (do
+        (- max-iterations 1)
+        (find-new-means old-means items distance features)))))
+
+;start with means -> find new means -> assign check difference -> return what you need
--- src/song_recommender/core.clj
+++ src/song_recommender/core.clj
@@ -7,17 +7,23 @@
 (def normalized-data
   (map #(data/row->normalized_row % global/non-numeric-features global/numeric-features) global/raw-data))
 
-
-(println (first normalized-data))
-(println (clustering/euclidean-distance-for-features (first normalized-data) (nth normalized-data 1) global/numeric-features))
-(println (clustering/find-closest-from-items (first normalized-data) normalized-data clustering/euclidean-distance-for-features global/numeric-features))
+;
+;(println (first normalized-data))
+;(println (clustering/euclidean-distance-for-features (first normalized-data) (nth normalized-data 1) global/numeric-features))
+;(println (clustering/find-closest-from-items (first normalized-data) normalized-data clustering/euclidean-distance-for-features global/numeric-features))
 
 
 (def means (take 5 normalized-data))
 (def subset (take 20 normalized-data))
 
 
-(println (clustering/assign-to-means means subset clustering/euclidean-distance-for-features global/numeric-features))
-(println (clustering/average-point means global/numeric-features))
+;(println (clustering/assign-to-means means subset clustering/euclidean-distance-for-features global/numeric-features))
+;(println (clustering/average-point means global/numeric-features))
+;(println (clustering/average-item means global/numeric-features clustering/euclidean-distance-for-features))
 
-(println (clustering/average-item means global/numeric-features clustering/euclidean-distance-for-features))
+
+;(println (map :remote_id means))
+
+;(println (map :remote_id (clustering/find-new-means means subset clustering/euclidean-distance-for-features global/numeric-features)))
+
+(println (clustering/calculate-centroids means subset clustering/euclidean-distance-for-features global/numeric-features))
