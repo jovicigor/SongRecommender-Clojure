492f3b0a4e409c1918c646aca4e933a5b23770f8
= <jovic.i24@gmail.com>
2017-04-13 15:03:55 +0200

    introduce random means

--- src/config/global.clj
+++ src/config/global.clj
@@ -1,17 +1,11 @@
 (ns config.global
   (:require [semantic-csv.core :as sc]))
 
-
 ;sc/slurp-csv returns squence of dictionaries
 (defn read-data [path-to-csv]
   (sc/slurp-csv path-to-csv))
-
-(def raw-data
-  (read-data "src/10KSongs.csv"))
 
 (def numeric-features '(:acousticness :danceability :energy :instrumentalness :track_key
                          :liveness :mode :popularity :speechiness :tempo :valence :album_year))
 
 (def non-numeric-features '(:remote_id :genres))
-
-
--- src/core/core.clj
+++ src/core/core.clj
@@ -3,7 +3,7 @@
             [machine_learning.data-manipulation :as data]
             [machine_learning.clustering :as clustering])
   (:gen-class :name com.example.SongRecommenderCore
-              :methods [#^{:static true} [performClustering [int] java.util.Map]]))
+              :methods [#^{:static true} [performClustering [int java.lang.String] java.util.Map]]))
 
 
 (defn get-nummeric-values [feature, data]
@@ -17,13 +17,15 @@
 (defn calculate-per-feature [reducer data]
   (into {} (map #(vector % (get-for-feature reducer % data)) global/numeric-features)))
 
+(defn take-random-sample [n collection]
+  (map #(let [num %] (rand-nth collection)) (range n)))
+
 ;returns map: CentroidId: IdsOfItemsInsideCluster
 (defn -performClustering [num-of-clusters path-to-csv]
   (let [data (global/read-data path-to-csv)
         min_per_feature (calculate-per-feature min data)
         max_per_feature (calculate-per-feature max data)
         normalized-data (map #(data/row->normalized_row % global/non-numeric-features global/numeric-features min_per_feature max_per_feature) data)
-        means (take num-of-clusters normalized-data)]
+        means (take-random-sample num-of-clusters normalized-data)]
     (clustering/kmeans means normalized-data clustering/euclidean-distance-for-features global/numeric-features)))
 
-(println (first (-performClustering 4 "src/10KSongs.csv")))
