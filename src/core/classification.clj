da0b5bd694bc005706c4a59e6c2bc9e7c9e4addd
= <jovic.i24@gmail.com>
2017-04-16 19:19:25 +0200

    implement find centroids for a song

---
+++ src/core/classification.clj
@@ -0,0 +1,23 @@
+(ns core.classification
+  (:require [config.global :as global]
+            [machine_learning.data-manipulation :as data])
+  (:gen-class :name com.songrecommender.Classifier
+              :methods [[findClosestCentroids [java.lang.String java.lang.String java.lang.String java.util.Map] java.lang.String]
+                        [findSimilarWithEuclideanDistance [java.lang.String java.lang.String java.lang.String java.util.Map] java.lang.String]]))
+
+
+(defn -findClosestCentroids [this path-to-cenroids path-to-min-csv path-to-max-csv song]
+  (let [centroids (global/read-data path-to-cenroids)
+        min_per_feature (global/calculate-per-feature min (global/read-data path-to-min-csv))
+        max_per_feature (global/calculate-per-feature max (global/read-data path-to-max-csv))
+        normalized-centroids (map #(data/row->normalized_row % global/non-numeric-features global/numeric-features min_per_feature max_per_feature) centroids)
+        normalized-song (data/row->normalized_row song global/non-numeric-features global/numeric-features min_per_feature max_per_feature)]
+    (:remote_id (global/find-closest-from-items normalized-song normalized-centroids global/euclidean-distance-for-features global/numeric-features))))
+
+(defn -findSimilarWithEuclideanDistance [this path-to-cluster path-to-min-csv path-to-max-csv song]
+  (let [cluster-items (global/read-data path-to-cluster)
+        min_per_feature (global/calculate-per-feature min (global/read-data path-to-min-csv))
+        max_per_feature (global/calculate-per-feature max (global/read-data path-to-max-csv))
+        normalized-cluster (map #(data/row->normalized_row % global/non-numeric-features global/numeric-features min_per_feature max_per_feature) cluster-items)
+        normalized-song (data/row->normalized_row song global/non-numeric-features global/numeric-features min_per_feature max_per_feature)]
+    (:remote_id (global/find-closest-from-items normalized-song normalized-cluster global/euclidean-distance-for-features global/numeric-features))))
--- src/machine_learning/clustering.clj
+++ src/machine_learning/clustering.clj
@@ -2,19 +2,8 @@
   (:require [config.global :as global]))
 
 
-(defn euclidean-distance [first-vector second-vector]
-  (Math/sqrt (reduce + (map #(Math/pow (- %1 %2) 2) first-vector second-vector))))
-
-(defn euclidean-distance-for-features [map1 map2 features]
-  (euclidean-distance (map #(% map1) features)
-                      (map #(% map2) features)))
-
-(defn find-closest-from-items [item items distance features]
-  (first (sort-by #(distance % item features) items)))
-
-
 (defn assign-to-means [means items distance features]
-  (group-by #(:remote_id (find-closest-from-items % means distance features)) items))
+  (group-by #(:remote_id (global/find-closest-from-items % means distance features)) items))
 
 
 ;this method should be in average-item method
@@ -26,7 +15,7 @@
 ;find average item from items for average point
 (defn average-item [items features distance]
   (let [average-point (average-point items features)]
-    (find-closest-from-items average-point items distance features)))
+    (global/find-closest-from-items average-point items distance features)))
 
 ;here you should map values to average-item
 (defn find-new-means [old-means items distance features]
