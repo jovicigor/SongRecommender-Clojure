0f4114c38b2fad2825a45a11242ca4bb43c4617c
= <jovic.i24@gmail.com>
2017-05-04 21:09:06 +0200

    minor functions rename according to styleguide

--- src/machine_learning/data_manipulation.clj
+++ src/machine_learning/data_manipulation.clj
@@ -1,20 +1,20 @@
 (ns machine_learning.data-manipulation
   (:require [config.global :as global]))
 
-(defn min_max_normalization [minimum maximum value]
+(defn min-max-normalization [minimum maximum value]
   (/
     (- (double value) minimum)
     (- maximum minimum)))
 
-(defn normalize_feature [value feature min_per_feature max_per_feature]
+(defn normalize-feature [value feature min_per_feature max_per_feature]
   (let [minimum (feature min_per_feature)
         maximum (feature max_per_feature)]
-    (min_max_normalization minimum
+    (min-max-normalization minimum
                            maximum
                            value)))
 
-(defn normalize_features [row features min_per_feature max_per_feature]
-  (map #(vector % (normalize_feature (read-string (% row)) % min_per_feature max_per_feature))
+(defn normalize-features [row features min_per_feature max_per_feature]
+  (map #(vector % (normalize-feature (read-string (% row)) % min_per_feature max_per_feature))
        features))
 
 (defn extract-features [row features]
@@ -25,4 +25,4 @@
   (into {}
         (concat
           (extract-features row features_not_to_normalize)
-          (normalize_features row features_to_normalize min_per_feature max_per_feature))))
+          (normalize-features row features_to_normalize min_per_feature max_per_feature))))
